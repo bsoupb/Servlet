59aa0e6a528bad684b5179bce8ee10f5038d34a0
LAPTOP-BTT3NSUQ\qotn0 <qotn0327@naver.com>
2024-01-20 22:08:12 +0900

    [배수빈] ex05 추가

---
+++ src/main/java/com/soup/servlet/servlet/ex/Ex05Controller.java
@@ -0,0 +1,37 @@
+package com.soup.servlet.servlet.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("servlet/ex05")
+public class Ex05Controller extends HttpServlet {
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		// 이름과 생년월일 전달 받고 이름과 나이를 html 로 rseponse에 담는다.
+		
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String birthday = request.getParameter("birthday"); // 20040812
+		
+		String yearString = birthday.substring(0, 4);
+		int year = Integer.parseInt(yearString);
+		
+		// 한국나이
+		int age = 2024 - year + 1;
+		
+		out.println("<html><head><title>이름 나이</title></head><body>");
+		out.println("<h2>이름 : " + name + "</h2>");
+		out.println("<h2>나이 : " + age + "</h2>");
+		out.println("</body></html>");
+	}
+}
